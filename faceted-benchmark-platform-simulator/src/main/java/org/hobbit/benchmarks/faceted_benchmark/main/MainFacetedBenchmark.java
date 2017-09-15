3621374f38c0e147a86d1ae9cb63d789b21306f2
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2017-09-15 15:54:26 +0200

    some more cleanup of the repo

---
+++ faceted-benchmark-platform-simulator/src/main/java/org/hobbit/benchmarks/faceted_benchmark/main/MainFacetedBenchmark.java
@@ -0,0 +1,9 @@
+package org.hobbit.benchmarks.faceted_benchmark.main;
+
+import org.hobbit.core.run.ComponentStarter;
+
+public class MainFacetedBenchmark {
+    public static void main(String[] args) {
+        ComponentStarter.main(new String[]{"org.hobbit.benchmark.FacetedBenchmarkController"});
+    }
+}
