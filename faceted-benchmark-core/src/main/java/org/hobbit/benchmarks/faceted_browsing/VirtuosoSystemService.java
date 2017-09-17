7203152b507e4819cc1be18b303328c12da13718
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2017-09-18 01:58:40 +0200

    factored out a class

--- faceted-benchmark-core/src/main/java/org/hobbit/benchmarks/faceted_browsing/VirtuosoSystemService.java
+++ faceted-benchmark-core/src/main/java/org/hobbit/benchmarks/faceted_browsing/VirtuosoSystemService.java
@@ -21,8 +21,6 @@ import org.ini4j.InvalidFileFormatException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.util.concurrent.AbstractIdleService;
-
 /**
  * A service wrapper instance manages a single underlying service process.
  *
@@ -32,7 +30,7 @@ import com.google.common.util.concurrent.AbstractIdleService;
  *
  */
 public class VirtuosoSystemService
-    extends AbstractIdleService
+    extends AbstractSystemService
     implements SparqlBasedSystemService
 {
     private static final Logger logger = LoggerFactory.getLogger(VirtuosoSystemService.class);
@@ -43,58 +41,10 @@ public class VirtuosoSystemService
     // The working directory. By default, the folder of the ini file
     protected Path workingPath;
 
-    protected Duration healthCheckInterval = Duration.ofSeconds(3);
-    // protected int healthCheckRetries = 10;
-
     // The parsed ini file, used to read out ports
     protected transient Ini virtIni;
     protected transient Preferences virtIniPrefs;
 
-    protected transient Process process;
-
-    protected Consumer<String> outputSink;
-
-    public Consumer<String> getOutputSink() {
-        return outputSink;
-    }
-
-    public VirtuosoSystemService setOutputSink(Consumer<String> outputSink) {
-        this.outputSink = outputSink;
-        return this;
-    }
-
-    public Duration getHealthCheckInterval() {
-        return healthCheckInterval;
-    }
-
-    public VirtuosoSystemService setHealthCheckInterval(Duration healthCheckInterval) {
-        this.healthCheckInterval = healthCheckInterval;
-        return this;
-    }
-
-    // public int getHealthCheckRetries() {
-    // return healthCheckRetries;
-    // }
-    //
-    // public VirtuosoServiceWrapper setHealthCheckRetries(int
-    // healthCheckRetries) {
-    // this.healthCheckRetries = healthCheckRetries;
-    // return this;
-    // }
-
-    // A single instance of the shutdown hook thread
-    protected Thread shutdownHookThread = new Thread(() -> {
-        // Not sure if we still have a logger during shutdown
-        System.err.println("Shutdown hook: terminating virtuoso process");
-        if(isRunning()) {
-            stopAsync();
-            try {
-                awaitTerminated(10, TimeUnit.SECONDS);
-            } catch (TimeoutException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    });
 
     public VirtuosoSystemService(Path virtExecPath, Path virtIniPath) {
         super();
@@ -102,6 +52,69 @@ public class VirtuosoSystemService
         this.virtIniPath = virtIniPath;
 
         this.workingPath = virtIniPath.getParent();
+    }
+
+    @Override
+    public VirtuosoSystemService setOutputSink(Consumer<String> outputSink) {
+        super.setOutputSink(outputSink);
+        return this;
+    }
+
+    @Override
+    public VirtuosoSystemService setHealthCheckInterval(Duration healthCheckInterval) {
+        super.setHealthCheckInterval(healthCheckInterval);
+        return this;
+    }
+
+    @Override
+    protected ProcessBuilder prepareProcessBuilder() {
+        ProcessBuilder result = new ProcessBuilder(virtExecPath.toString(), "-c", virtIniPath.toString(), "-f");
+        result.directory(workingPath.toFile());
+        return result;
+    }
+
+    /**
+     * Starts the service and yields a future indicating whether it became
+     * healthy within a certain time limit.
+     *
+     * The service may still become healthy at a later stage, however,
+     *
+     * @return
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @Override
+    protected void startUp() throws IOException, InterruptedException {
+        // Attempt to read the ini file
+        virtIni = new Ini(virtIniPath.toFile());
+        virtIniPrefs = new IniPreferences(virtIni);
+
+        super.startUp();
+    }
+
+
+    /**
+     * Perform a health check on the service
+     *
+     * @return
+     */
+    public boolean performHealthCheck() {
+        boolean result = false;
+        try (RDFConnection conn = createDefaultConnection()) {
+
+            // A very basic select query, which all SPARQL systems
+            // should be capable to process quickly, as usually it does not
+            // match anything
+            conn.querySelect("SELECT * { <http://example.org/healthcheck> a ?t }", (qs) -> {
+            });
+            result = true;
+        } catch (Exception e) {
+            logger.debug("Health check failed with reason: ", e);
+        }
+
+        logger.debug("Health check response: " + result);
+
+        return result;
     }
 
     /**
@@ -130,100 +143,6 @@ public class VirtuosoSystemService
         System.out.println(odbcPort + " --- " + httpPort);
 
         return result;
-    }
-
-    /**
-     * Perform a health check on the service
-     *
-     * @return
-     */
-    public boolean performHealthCheck() {
-        boolean result = false;
-        try (RDFConnection conn = createDefaultConnection()) {
-
-            // A very basic select query, which all SPARQL systems
-            // should be capable to process quickly, as usually it does not
-            // match anything
-            conn.querySelect("SELECT * { <http://example.org/healthcheck> a ?t }", (qs) -> {
-            });
-            result = true;
-        } catch (Exception e) {
-            logger.debug("Health check failed with reason: ", e);
-        }
-
-        logger.debug("Health check response: " + result);
-
-        return result;
-    }
-
-    /**
-     * Starts the service and yields a future indicating whether it became
-     * healthy within a certain time limit.
-     *
-     * The service may still become healthy at a later stage, however,
-     *
-     * @return
-     * @throws IOException
-     * @throws InterruptedException
-     */
-    @Override
-    protected void startUp() throws IOException, InterruptedException {
-        // Attempt to read the ini file
-        virtIni = new Ini(virtIniPath.toFile());
-        virtIniPrefs = new IniPreferences(virtIni);
-
-        Runtime.getRuntime().addShutdownHook(shutdownHookThread);
-
-        ProcessBuilder pb = new ProcessBuilder(virtExecPath.toString(), "-c", virtIniPath.toString(), "-f");
-        pb.directory(workingPath.toFile());
-        process = SimpleProcessExecutor.wrap(pb)
-                .setService(true)
-                // Delegate output to whatever the current sink is
-                .setOutputSink(x -> { if(outputSink != null) { outputSink.accept(x); }})
-                .execute();
-
-        // Some simple retry policy on the health check
-        // TODO Use async retry library (or something similar) for
-        // having a powerful api for crafting retry policies
-
-        // Start another thread that determines when the service becomes healthy
-        try {
-            boolean r = false;
-            // for(int i = 0; ; ++i) {
-            while (process.isAlive()) {
-                long millis = healthCheckInterval.toMillis();
-                Thread.sleep(millis);
-                r = performHealthCheck();
-                if (r) {
-                    break;
-                }
-            }
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-            // If startup gets interrupted, shut down the service
-            // throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    protected void shutDown() {
-
-        // Things may get better with Java 9, such as having an event when the process
-        // terminates which would enable us to exit immediately without polling delay
-        process.destroy();
-
-        try {
-            while (process.isAlive()) {
-                // Waiting for process to die
-                long millis = healthCheckInterval.toMillis();
-                Thread.sleep(millis);
-            }
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        process = null;
-        Runtime.getRuntime().removeShutdownHook(shutdownHookThread);
     }
 
     public static void main(String[] args) throws Exception {
