f48bde625ddf196d4e0deb67326e94c070e5b173
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2018-04-12 11:01:54 +0200

    moved some classes out to jsa

--- hobbit-sdk-parent/hobbit-sdk-core/src/main/java/org/aksw/jena_sparql_api/core/service/SparqlBasedSystemService.java
+++
@@ -1,9 +0,0 @@
-package org.aksw.jena_sparql_api.core.service;
-
-import org.aksw.commons.service.core.SystemService;
-
-public interface SparqlBasedSystemService
-    extends SparqlBasedService, SystemService
-{
-
-}
