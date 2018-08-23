0a5d71f0ae760c2fae577f46ffbefcb96d0dfc3a
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2018-08-23 15:42:36 +0200

    work on registering and lauching from the docker service registry

--- hobbit-sdk-parent/hobbit-sdk-qpid7/src/main/java/org/hobbit/qpid/v7/plugin/hobbit/HobbitPluginQpid7Utils.java
+++ hobbit-sdk-parent/hobbit-sdk-qpid7/src/main/java/org/hobbit/qpid/v7/plugin/hobbit/HobbitPluginQpid7Utils.java
@@ -1,5 +1,24 @@
 package org.hobbit.qpid.v7.plugin.hobbit;
 
-public class HobbitPluginQpid7Utils {
+import java.util.function.Supplier;
 
+import org.hobbit.qpid.v7.config.ConfigQpidBroker;
+import org.hobbit.sdk.docker.registry.DockerServiceRegistryImpl;
+import org.springframework.boot.Banner;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+public class HobbitPluginQpid7Utils {
+	public static void init() {
+		Supplier<SpringApplicationBuilder> appBuilderSupplier = () -> new SpringApplicationBuilder()
+				.sources(ConfigQpidBroker.class)
+				//.sources(ApplicationRunnerQpidBroker.class)
+				.bannerMode(Banner.Mode.OFF);
+		
+		
+		DockerServiceRegistryImpl.get().getServiceFactoryMap()
+			.put("git.project-hobbit.eu:4567/cstadler/faceted-browsing-benchmark-releases/hobbit-sdk-qpid7",
+			null
+					//() -> new Docker					
+			);
+	}
 }
