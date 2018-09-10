aaecb6ea166c6c16b1a56029d36f8876559b621a
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2018-09-10 12:26:42 +0200

    Cleaned up the main file

--- faceted-browsing-benchmark-parent/faceted-browsing-benchmark-v2-parent/faceted-browsing-benchmark-v2-core/src/main/java/org/hobbit/benchmark/faceted_browsing/v2/main/MainTestPavelsDataGen.java
+++ faceted-browsing-benchmark-parent/faceted-browsing-benchmark-v2-parent/faceted-browsing-benchmark-v2-core/src/main/java/org/hobbit/benchmark/faceted_browsing/v2/main/MainTestPavelsDataGen.java
@@ -37,67 +37,71 @@ import com.google.common.collect.Lists;
 import io.reactivex.Flowable;
 import io.reactivex.processors.PublishProcessor;
 
+/**
+ * This class eventually launches a data generator (DG) and processes the emitted messages.
+ * Setting of the DG requires - among other things -
+ * launching an AMQP server, a SPARQL endpoint, and a task generator to receive the messages.
+ * 
+ * 
+ * @param args
+ * @throws Exception
+ */
 public class MainTestPavelsDataGen {
 	private static final Logger logger = LoggerFactory.getLogger(MainTestPavelsDataGen.class);
 
 	
 	public static void main(String[] args) throws Exception {
 		try (DockerServiceFactory<?> dsf = DockerServiceFactoryDockerClient.create(true, Collections.emptyMap(), Collections.emptySet())) {
+			// Create a session id (used in naming of the amqp communication
+			// channels to avoid conflicts between different sessions)
 			String sessionId = "testsession" + "." + RabbitMqFlows.idGenerator.get();
 	
-			
-			DockerService amqpService = dsf.create("git.project-hobbit.eu:4567/cstadler/faceted-browsing-benchmark-releases/hobbit-sdk-qpid7",
+			// Configure the AMQP server
+			DockerService amqpService =dsf
+					.create("git.project-hobbit.eu:4567/cstadler/faceted-browsing-benchmark-releases/hobbit-sdk-qpid7",
 							ImmutableMap.<String, String>builder().build());
-	//		Service amqpService = new ServiceSpringApplicationBuilder("qpid-server", new SpringApplicationBuilder()
-	//				// Add the amqp broker
-	//				.properties(new ImmutableMap.Builder<String, Object>()
-	//						.put("hostMode", true)
-	//						.put(Constants.HOBBIT_SESSION_ID_KEY, sessionId)
-	//						//.put(ConfigRabbitMqConnectionFactory.AMQP_VHOST, "default")
-	//						.build())
-	//				.sources(ConfigQpidBroker.class)
-	//				.sources(ServiceNoOp.class))
-	//				;
 	
 			logger.info("AMQP server starting ...");
 			
 			try {
+				// Start it and obtain the host name
 				amqpService.startAsync().awaitRunning(10, TimeUnit.SECONDS);
-	
 				String amqpHost = amqpService.getContainerId();
 				
 				logger.info("AMQP server started and online at " + amqpHost);
 				
+				// Set up the container for the preloaded SPARQL enpdoint
+				// Also, add a health check to ensure that the 'runnig' state means that
+				// the SPARQL enppoint is up - rather than only the container
 				DockerService dbService = ComponentUtils.wrapSparqlServiceWithHealthCheck(
 						dsf.create("git.project-hobbit.eu:4567/cstadler/faceted-browsing-benchmark-releases/linkedgeodata-20180719-germany-building",
-		//				dsf.create("tenforce/virtuoso",
 								ImmutableMap.<String, String>builder()
 								.put("SPARQL_UPDATE", "true")
 								.build()),
 						8890);
 		
 				try {
+					// Start up the SPARQL endpoint
 					dbService.startAsync().awaitRunning(60, TimeUnit.SECONDS);
 					String sparqlApiBase = "http://" + dbService.getContainerId() + ":8890/";
 					String sparqlEndpoint = sparqlApiBase + "sparql";
 					
 					logger.info("Sparql endpoint online at " + sparqlEndpoint);
 			
+					// Configure a connection to the SPARQL endpoint
 					RDFConnection conn = RDFConnectionFactory.connect(sparqlEndpoint);
-					// Set up a flow that transform insert requests of a collection of quads into
-					// corresponding update requests
+
+					// Set up a flow that transforms SPARQL insert requests of a collection
+					// of quads into corresponding update requests
 					PublishProcessor<Collection<Quad>> quadsInserter = PublishProcessor.create();
 			
 					// ... thereby remove old records once the data grows too large
 					int expectedModelSize = 3;
+
 					SimpleSparqlInsertRequestFactory insertHandler = new SimpleSparqlInsertRequestFactoryWindowedInMemory(expectedModelSize);
 			
 					quadsInserter
-						//.map(SetDatasetGraph::new)
 						.map(insertHandler::createUpdateRequest)
-			//			.doOnNext(ur -> UpdateRequestUtils.applyWithIri(ur, "http://www.example.org/"))
-			//			.doOnNext(ur -> System.out.println("Request: " + ur))
-			//			.doOnNext(conn::update)
 						.subscribe(ur -> {
 							System.out.println("Request: " + ur);
 							try {
@@ -108,6 +112,7 @@ public class MainTestPavelsDataGen {
 						}, t -> logger.warn("Failed update: " + t));
 			
 					
+					// Configure the data generator container
 					DockerService dgService = dsf.create("git.project-hobbit.eu:4567/smirnp/grow-smarter-benchmark/datagen", ImmutableMap.<String, String>builder()
 							.put(Constants.RABBIT_MQ_HOST_NAME_KEY, amqpHost)
 							.put(Constants.HOBBIT_SESSION_ID_KEY, sessionId)
@@ -124,15 +129,11 @@ public class MainTestPavelsDataGen {
 							.put("SPARQL_ENDPOINT_URL", sparqlEndpoint)
 							.build());
 			
-			//		HOUSES_COUNT=1
-			//				DEVICES_PER_HOUSEHOLD_MIN=1
-			//				DEVICES_PER_HOUSEHOLD_MAX=10
-			//				SENSORS_PER_DEVICE=10
-			//				ITERATIONS_LIMIT=10
-			//				DATA_SENDING_PERIOD_MS=1000
-			//				OUTPUT_FORMAT=RDF
-			//				SPARQL_ENDPOINT_URL=http://10.67.1.30:8890/sparql
-			
+					// Create a dummy task generator image
+					// Note: ServiceNoOp is a service that does nothing by itself - however
+					// the life cycle of the spring context is bound to it - hence
+					// stopping the service tears down the context and consequently
+					// frees all associated resources, such as AMQP connections
 					ServiceSpringApplicationBuilder tgService = new ServiceSpringApplicationBuilder("tg", ComponentUtils.createComponentBaseConfig("tg", Constants.CONTAINER_TYPE_BENCHMARK)
 							.properties(ImmutableMap.<String, Object>builder()
 									.put(Constants.RABBIT_MQ_HOST_NAME_KEY, amqpHost)
@@ -144,21 +145,14 @@ public class MainTestPavelsDataGen {
 					logger.info("TG starting ...");
 					try {
 						tgService.startAsync().awaitRunning(10, TimeUnit.SECONDS);
-				
 						
 						Flowable<ByteBuffer> flow = (Flowable<ByteBuffer>)tgService.getAppBuilder().context().getBean("dg2tgReceiver");	
 						System.out.println("Flow " + flow);
-				//		flow.onne
-				
-						
 				
 						logger.info("TG started - obtained receiver " + flow);
 						flow.subscribe(msg -> {
-				//			System.out.println("Got message:");
-				//			System.out.println(new String(msg.array(), StandardCharsets.UTF_8));	
-				//			System.out.println();
-				//			System.out.println();
-				
+							// Parse messages as RDF models and pass them to the inserter
+							
 							Model m = ModelFactory.createDefaultModel();
 							RDFDataMgr.read(m, new ByteArrayInputStream(msg.array()), null, Lang.NTRIPLES);
 							System.out.println("Got model with " + m.size() + " triples");
@@ -170,9 +164,9 @@ public class MainTestPavelsDataGen {
 							List<Quad> quads = Lists.newArrayList(ds.asDatasetGraph().find());
 							quadsInserter.onNext(quads);
 							
-						});//RabbitMQUtils.readModel(msg.duplicate())));
-						//flow.subscribe(msg -> System.out.println("Got message"));
-						
+						});
+
+						// Start the data generator
 						logger.info("DG starting ...");
 						try {
 							dgService.startAsync().awaitRunning(10, TimeUnit.SECONDS);
@@ -180,13 +174,18 @@ public class MainTestPavelsDataGen {
 							
 							logger.info("DG started");
 										
+							// Obtain the command sender from the spring context ...
 							Subscriber<ByteBuffer> commandSender = (Subscriber<ByteBuffer>)tgService.getAppBuilder().context().getBean("commandSender");	
 					
+							// ... and send the the command to start data generation
 					        commandSender.onNext(ByteBuffer.wrap(new byte[]{Commands.DATA_GENERATOR_START_SIGNAL}));
-					
-							
+												        
 							logger.info("DG termination awaited");
 							dgService.awaitTerminated(30, TimeUnit.SECONDS);
+
+							// Done - tear down everything in order
+							// E.g. the amqp server has to shut down last, so that components
+							// can inform each other about them shutting down
 
 						} finally {
 							dgService.stopAsync().awaitTerminated(10, TimeUnit.SECONDS);
@@ -205,3 +204,15 @@ public class MainTestPavelsDataGen {
 		logger.info("Done");
 	}
 }
+
+// FIXME Consider deletion of  comments below
+//		HOUSES_COUNT=1
+//				DEVICES_PER_HOUSEHOLD_MIN=1
+//				DEVICES_PER_HOUSEHOLD_MAX=10
+//				SENSORS_PER_DEVICE=10
+//				ITERATIONS_LIMIT=10
+//				DATA_SENDING_PERIOD_MS=1000
+//				OUTPUT_FORMAT=RDF
+//				SPARQL_ENDPOINT_URL=http://10.67.1.30:8890/sparql
+
+		
