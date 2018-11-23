559e1128e5fafffaf8c489370a99e173a01e759c
Simon Bin <sbin@informatik.uni-leipzig.de>
2018-11-23 13:21:05 +0100

    test cP2

--- faceted-browsing-benchmark-parent/faceted-browsing-benchmark-v2-parent/faceted-browsing-benchmark-v2-core/src/test/java/org/hobbit/benchmark/faceted_browsing/v2/TestFacetedQuery2.java
+++ faceted-browsing-benchmark-parent/faceted-browsing-benchmark-v2-parent/faceted-browsing-benchmark-v2-core/src/test/java/org/hobbit/benchmark/faceted_browsing/v2/TestFacetedQuery2.java
@@ -9,7 +9,6 @@ import org.aksw.facete.v3.bgp.api.XFacetedQuery;
 import org.aksw.facete.v3.impl.FacetNodeImpl;
 import org.aksw.facete.v3.impl.FacetedQueryImpl;
 import org.aksw.jena_sparql_api.utils.NodeHolder;
-import org.apache.jena.graph.Node;
 import org.apache.jena.query.DatasetFactory;
 import org.apache.jena.query.Query;
 import org.apache.jena.rdf.model.Model;
@@ -23,7 +22,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
@@ -46,6 +44,38 @@ public class TestFacetedQuery2 {
 		//FacetedQueryResource fq = FacetedQueryImpl.create(model, conn);
 	}
 
+	static String getQueryPattern(FacetNode node) {
+		return ((FacetNodeImpl) node).createValueQuery(false).toConstructQuery().getValue().getQueryPattern().toString();
+	}
+
+	@Test
+	public void testCp2() {
+		final TaskGenerator taskGenerator = new TaskGenerator(null, null);
+		taskGenerator.setPseudoRandom(new Random(1234l));
+		final FacetNode node = fq.root();
+
+		assertEquals( "{ ?v_1  ?p  ?o }" ,
+				getQueryPattern(node) );
+
+		taskGenerator.applyCp2(node);
+
+		assertEquals( "{ { ?v_1  <http://www.example.org/locatedIn>  ?v_2\n" +
+				"    FILTER bound(?v_2)\n" +
+				"  }\n" +
+				"  ?v_1  ?p  ?o\n" +
+				"}" , getQueryPattern(node) );
+
+		taskGenerator.applyCp2(node);
+
+		assertEquals( "{ { ?v_1  <http://www.example.org/population>  ?v_2 ;\n" +
+				"          <http://www.example.org/locatedIn>  ?v_3\n" +
+				"    FILTER bound(?v_3)\n" +
+				"    FILTER bound(?v_2)\n" +
+				"  }\n" +
+				"  ?v_1  ?p  ?o\n" +
+				"}" , getQueryPattern(node) );
+	}
+
 	@Test
 	public void testCp1() {
 		final TaskGenerator taskGenerator = new TaskGenerator(null, null);
@@ -60,7 +90,7 @@ public class TestFacetedQuery2 {
 		assertEquals( "{ ?v_1  <http://www.example.org/population>  500000\n" +
 						"  { ?v_1  ?p  ?o }\n" +
 						"}" ,
-				((FacetNodeImpl) node).createValueQuery(false).toConstructQuery().getValue().getQueryPattern().toString()
+				getQueryPattern(node)
 		);
 
 		//System.out.println("---");
@@ -69,7 +99,7 @@ public class TestFacetedQuery2 {
 						"        <http://www.example.org/population>  500000\n" +
 						"  { ?v_1  ?p  ?o }\n" +
 						"}" ,
-				((FacetNodeImpl) node).createValueQuery(false).toConstructQuery().getValue().getQueryPattern().toString()
+				getQueryPattern(node)
 		);
 
 	}
@@ -79,7 +109,7 @@ public class TestFacetedQuery2 {
 	public void testRangeConstraint() {
 		//final DataQuery<FacetCount> facetCountDataQuery = fq.root().fwd().facetCounts();
 		//
-		final FacetNode facetCountDataQuery = fq.root()
+		final FacetNode node = fq.root()
 				.fwd("http://www.example.org/population")
 				.one()
 				.constraints()
@@ -91,18 +121,13 @@ public class TestFacetedQuery2 {
 				.parent()
 				;
 
-		final DataQuery<?> valueQuery = ((FacetNodeImpl) facetCountDataQuery).createValueQuery(false);
-		final Map.Entry<Node, Query> x = valueQuery.toConstructQuery();
-
-		assertEquals( "SELECT DISTINCT  ?v_1\n" +
-				"WHERE\n" +
-				"  { { ?v_1  <http://www.example.org/population>  ?v_2\n" +
-				"      FILTER ( ?v_2 <= 80000000 )\n" +
-				"      FILTER ( ?v_2 >= 50000 )\n" +
-				"    }\n" +
-				"    ?v_1  ?p  ?o\n" +
-				"  }\n" ,
-				x.getValue().toString() );
+		assertEquals( "{ { ?v_1  <http://www.example.org/population>  ?v_2\n" +
+						"    FILTER ( ?v_2 <= 80000000 )\n" +
+						"    FILTER ( ?v_2 >= 50000 )\n" +
+						"  }\n" +
+						"  ?v_1  ?p  ?o\n" +
+						"}" ,
+				getQueryPattern(node) );
 	}
 
 	@Test
