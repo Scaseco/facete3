f0fce01c6d4437977f195cbb228bfbf01811cec5
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2019-01-03 02:14:27 +0100

    added another test case for an issue with disappearing facets ; reason is that the base concept is apparently not injected

--- faceted-browsing-benchmark-parent/faceted-browsing-benchmark-v2-parent/faceted-browsing-benchmark-v2-core/src/test/java/org/hobbit/benchmark/faceted_browsing/v2/TestFacetedQueryAbsentFacetValues.java
+++ faceted-browsing-benchmark-parent/faceted-browsing-benchmark-v2-parent/faceted-browsing-benchmark-v2-core/src/test/java/org/hobbit/benchmark/faceted_browsing/v2/TestFacetedQueryAbsentFacetValues.java
@@ -6,7 +6,9 @@ import org.aksw.facete.v3.api.FacetDirNode;
 import org.aksw.facete.v3.api.FacetValueCount;
 import org.aksw.facete.v3.api.FacetedQuery;
 import org.aksw.facete.v3.impl.FacetedQueryBuilder;
+import org.aksw.jena_sparql_api.concepts.Concept;
 import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.shared.PrefixMapping;
 import org.apache.jena.vocabulary.OWL;
 import org.apache.jena.vocabulary.RDF;
 import org.apache.jena.vocabulary.RDFS;
@@ -14,6 +16,32 @@ import org.junit.Test;
 
 public class TestFacetedQueryAbsentFacetValues {
 
+	@Test
+	public void testAbsentConstraintIssueDisappearingFacet() {
+		FacetedQuery fq =
+				FacetedQueryBuilder.builder()
+					.configDataConnection().setSource(RDFDataMgr.loadModel(TestFacetedQuery2.DS_S_L_IN_G)).end()
+				.create();
+
+		fq.baseConcept(Concept.parse("?s | ?s a eg:Foobar", PrefixMapping.Extended));
+		fq.focus().fwd(RDF.type).one().constraints().eqIri("http://www.example.org/City").activate();
+		fq.focus().fwd(RDF.type).one().constraints().eqIri("http://www.example.org/Country").activate();
+		fq.focus().fwd("http://www.example.org/contains").one().constraints().absent().activate();
+
+		// We are expecting 2 facet values for the type property:
+		// [Germany, Leipzig]
+
+		// Due to the issue we get an empty result
+		
+		List<FacetValueCount> fvcs = fq.focus().fwd().facetValueCountsWithAbsent(true).only(RDF.type).exec().toList().blockingGet();
+		System.out.println(fvcs);
+		
+		//System.out.println("Facet counts: " + fq.focus().fwd().facetCounts(true).only("http://www.example.org/contains").exec().toList().blockingGet());
+		
+	}
+
+	
+	// This issue was fixed, turn into a proper test case
 	@Test
 	public void testAbsentConstraintIssueSpottedInFacete3() {
 		FacetedQuery fq =
