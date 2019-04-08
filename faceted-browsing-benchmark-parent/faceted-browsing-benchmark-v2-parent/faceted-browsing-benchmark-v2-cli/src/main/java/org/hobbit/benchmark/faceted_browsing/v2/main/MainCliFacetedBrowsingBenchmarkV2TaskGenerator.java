ea18139c6853301e1116e695874fe5a0ef738cf4
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2019-04-08 15:47:34 +0200

    Added dockerized virtuoso to generation tool

--- faceted-browsing-benchmark-parent/faceted-browsing-benchmark-v2-parent/faceted-browsing-benchmark-v2-cli/src/main/java/org/hobbit/benchmark/faceted_browsing/v2/main/MainCliFacetedBrowsingBenchmarkV2TaskGenerator.java
+++ faceted-browsing-benchmark-parent/faceted-browsing-benchmark-v2-parent/faceted-browsing-benchmark-v2-cli/src/main/java/org/hobbit/benchmark/faceted_browsing/v2/main/MainCliFacetedBrowsingBenchmarkV2TaskGenerator.java
@@ -1,17 +1,26 @@
 package org.hobbit.benchmark.faceted_browsing.v2.main;
 
+import java.io.File;
 import java.io.OutputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import org.aksw.commons.util.strings.StringUtils;
+import org.aksw.jena_sparql_api.core.FluentQueryExecutionFactory;
 import org.aksw.jena_sparql_api.core.RDFConnectionEx;
 import org.aksw.jena_sparql_api.core.RDFConnectionFactoryEx;
 import org.aksw.jena_sparql_api.core.RDFConnectionMetaData;
+import org.aksw.jena_sparql_api.core.connection.QueryExecutionFactorySparqlQueryConnection;
+import org.aksw.jena_sparql_api.core.connection.SparqlQueryConnectionJsa;
+import org.aksw.jena_sparql_api.ext.virtuoso.VirtuosoBulkLoad;
 import org.aksw.jena_sparql_api.mapper.proxy.JenaPluginUtils;
 import org.aksw.jena_sparql_api.utils.model.ResourceUtils;
 import org.apache.jena.query.Dataset;
@@ -22,22 +31,34 @@ import org.apache.jena.query.ResultSetFormatter;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdfconnection.RDFConnection;
 import org.apache.jena.rdfconnection.RDFConnectionFactory;
+import org.apache.jena.rdfconnection.RDFConnectionModular;
 import org.apache.jena.rdfconnection.SparqlQueryConnection;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
+import org.apache.jena.riot.WebContent;
 import org.apache.jena.sparql.core.DatasetDescription;
+import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
 import org.apache.jena.sparql.resultset.ResultSetMem;
 import org.hobbit.benchmark.faceted_browsing.component.FacetedBrowsingEncoders;
 import org.hobbit.benchmark.faceted_browsing.component.FacetedBrowsingVocab;
+import org.hobbit.benchmark.faceted_browsing.config.ComponentUtils;
 import org.hobbit.benchmark.faceted_browsing.v2.task_generator.TaskGenerator;
 import org.hobbit.benchmark.faceted_browsing.v2.task_generator.nfa.ScenarioConfig;
 import org.hobbit.core.component.BenchmarkVocab;
+import org.hobbit.core.service.docker.api.DockerService;
+import org.hobbit.core.service.docker.impl.docker_client.DockerServiceDockerClient;
+import org.hobbit.core.service.docker.impl.docker_client.DockerServiceSystemDockerClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.beust.jcommander.JCommander;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ObjectArrays;
+import com.spotify.docker.client.DockerClient;
+
+import virtuoso.jdbc4.VirtuosoDataSource;
 
 
 
@@ -55,105 +76,187 @@ public class MainCliFacetedBrowsingBenchmarkV2TaskGenerator {
 		
 
 	
+	
 	public static void main(String[] args) throws Exception {
 	
-		//PropertyBasedAcFluent.create();
-		
-		// HACK/WORKAROUND for Jcommander issue
-		// https://github.com/cbeust/jcommander/issues/464
-		// Add a dummy element to initialize a list property
-		args = ObjectArrays.concat(new String[] {"-d", "foo"}, args, String.class);
-		
-		JenaPluginUtils.registerJenaResourceClasses(
-				CommandMain.class,
-				RDFConnectionMetaData.class);
-		
-		JenaPluginUtils.registerJenaResourceClassesUsingPackageScan(ScenarioConfig.class);
-		
-//		Expr e = ExprUtils.parse("xsd:integer(floor(1.2)", PrefixMapping.Extended);
-//		System.out.println(e);
-//		NodeValue x = e.eval(BindingFactory.root(), FunctionEnvBase.createTest());
-//		System.out.println(x);
-//System.out.println("yay");
-//		Expr ffs = new E_Function(XSD.xint.getURI(), new ExprList(NodeValue.makeDecimal(1.2))); 
-//		System.out.println(ffs.eval(BindingFactory.root(), FunctionEnvBase.createTest()));
-//		if(true) { return; }
-		
-		CommandMain cmMain = ModelFactory.createDefaultModel().createResource().as(CommandMain.class);
+		DockerServiceSystemDockerClient dss = null;
+		DockerService ds = null;
 
-		JCommander jc = JCommander.newBuilder()
-				.addObject(cmMain)
-				.build();
-		          
-		
-		jc.parse(args);
-		
-		if(cmMain.getHelp()) {
-			jc.usage();
-			return;
-		}
-		
-		RDFConnectionEx connTmp;
-		
-		List<String> nonOptionArgs = cmMain.getNonOptionArgs();
-		String sparqEndpoint = cmMain.getSparqlEndpoint();
-
-		if(!nonOptionArgs.isEmpty()) {
-			
-			// Make paths absolute
-			List<Path> paths = nonOptionArgs.stream()
-					.map(Paths::get)
-					.map(Path::toAbsolutePath)
-					.collect(Collectors.toList());
-			
-			if(sparqEndpoint != null) {
-				throw new RuntimeException("Cannot mix file and sparql enpdoint sources");
-			}
-			
-			Model model = ModelFactory.createDefaultModel();
-			for(Path arg : paths) {
-				Model contrib = RDFDataMgr.loadModel(arg.toString());
-				model.add(contrib);
-			}
-			
-			RDFConnectionMetaData metadata = ModelFactory.createDefaultModel()
-					.createResource().as(RDFConnectionMetaData.class);
-
-			String id = paths.stream().map(Object::toString).collect(Collectors.joining("---"));
-			id = StringUtils.urlEncode(id);
-			id = id.replaceAll("%2F", "_");
-			metadata.setServiceURL(id);
-
-			
-			connTmp = RDFConnectionFactoryEx.wrap(
-					RDFConnectionFactory.connect(DatasetFactory.wrap(model)),
-					metadata);			
-//			if(cmMain.getNonOptionArgs().size() > 1) {
-//				throw new RuntimeException("Only 1 non-option argument expected");
-//			}
-		} else {
-			
-//			String sparqEndpoint = cmMain.getSparqlEndpoint();
+		try {
 	
-			DatasetDescription datasetDescription = new DatasetDescription();
-			datasetDescription.addAllDefaultGraphURIs(cmMain.getDefaultGraphUris());
+			//PropertyBasedAcFluent.create();
 			
-					
-					//RDFConnectionFactory.connect(sparqEndpoint);
+			// HACK/WORKAROUND for Jcommander issue
+			// https://github.com/cbeust/jcommander/issues/464
+			// Add a dummy element to initialize a list property
+			args = ObjectArrays.concat(new String[] {"-d", "foo"}, args, String.class);
+			
+			JenaPluginUtils.registerJenaResourceClasses(
+					CommandMain.class,
+					RDFConnectionMetaData.class);
+			
+			JenaPluginUtils.registerJenaResourceClassesUsingPackageScan(ScenarioConfig.class);
+			
+	//		Expr e = ExprUtils.parse("xsd:integer(floor(1.2)", PrefixMapping.Extended);
+	//		System.out.println(e);
+	//		NodeValue x = e.eval(BindingFactory.root(), FunctionEnvBase.createTest());
+	//		System.out.println(x);
+	//System.out.println("yay");
+	//		Expr ffs = new E_Function(XSD.xint.getURI(), new ExprList(NodeValue.makeDecimal(1.2))); 
+	//		System.out.println(ffs.eval(BindingFactory.root(), FunctionEnvBase.createTest()));
+	//		if(true) { return; }
+			
+			CommandMain cmMain = ModelFactory.createDefaultModel().createResource().as(CommandMain.class);
+	
+			JCommander jc = JCommander.newBuilder()
+					.addObject(cmMain)
+					.build();
+			          
+			
+			jc.parse(args);
+			
+			if(cmMain.getHelp()) {
+				jc.usage();
+				return;
+			}
+			
+			RDFConnectionEx rawConnEx;
+			
+			List<String> nonOptionArgs = cmMain.getNonOptionArgs();
+			String sparqEndpoint = cmMain.getSparqlEndpoint();
+	
+			if(!nonOptionArgs.isEmpty()) {
+				
+				DatasetDescription datasetDescription = new DatasetDescription();
+				datasetDescription.addDefaultGraphURI("http://www.example.org/");
+				
+				// Make paths absolute
+				List<Path> paths = nonOptionArgs.stream()
+						.map(Paths::get)
+						.map(Path::toAbsolutePath)
+						.collect(Collectors.toList());
+				
+				
+				if(sparqEndpoint != null) {
+					throw new RuntimeException("Cannot mix file and sparql enpdoint sources");
+				}
+				
+//				Model model = ModelFactory.createDefaultModel();
+//				for(Path arg : paths) {
+//					Model contrib = RDFDataMgr.loadModel(arg.toString());
+//					model.add(contrib);
+//				}
+				
+				RDFConnectionMetaData metadata = ModelFactory.createDefaultModel()
+						.createResource().as(RDFConnectionMetaData.class);
+	
+				String id = paths.stream().map(Object::toString).collect(Collectors.joining("---"));
+				id = StringUtils.urlEncode(id);
+				id = id.replaceAll("%2F", "_");
+				metadata.setServiceURL(id);
+				metadata.getDefaultGraphs().addAll(datasetDescription.getDefaultGraphURIs());
+				metadata.getNamedGraphs().addAll(datasetDescription.getNamedGraphURIs());
+				
+				
+/// begin of DOCKER STUFF
 
-			connTmp = RDFConnectionFactoryEx.connect(sparqEndpoint, datasetDescription);
-		}				
-		
-		String dataSummaryUri = cmMain.getPathFindingDataSummaryUri();
-		Model dataSummaryModel = null;
-		if(dataSummaryUri != null) {
-			logger.info("Loading path finding data summary from " + dataSummaryUri);
-			dataSummaryModel = RDFDataMgr.loadModel(dataSummaryUri);
-			logger.info("Done loading path finding data summary from" + dataSummaryUri);
-		}
-		
-		
-		try(RDFConnectionEx conn = connTmp) {
+				dss = DockerServiceSystemDockerClient.create(true, Collections.emptyMap(), Collections.emptySet());
+				DockerClient dockerClient = dss.getDockerClient();
+				
+				//conn = DatasetFactory.wrap(model);
+				
+				logger.info("Attempting to starting a virtuoso from docker");
+				DockerServiceDockerClient dsCore = dss.create("tenforce/virtuoso", ImmutableMap.<String, String>builder()
+						.put("SPARQL_UPDATE", "true")
+						.put("DEFAULT_GRAPH", "http://www.example.org/")
+						.build());
+								
+				ds = ComponentUtils.wrapSparqlServiceWithHealthCheck(dsCore, 8890);
+	
+				ds.startAsync().awaitRunning();
+	
+				// Give the sparql endpoint 5 seconds to come up
+				// TODO Add another example for wrapping with health checks
+				//Thread.sleep(10000);
+				
+				String dockerContainerId = dsCore.getCreationId();
+				String dockerContainerIp = ds.getContainerId();
+				
+				Path unzipFolder = Paths.get("/tmp").resolve(dockerContainerId);
+				Files.createDirectories(unzipFolder);
+				
+				for(Path path : paths) {
+					Path filename = path.getFileName();
+					Path target = unzipFolder.resolve(filename);
+					Files.copy(path, target);
+				}
+
+				
+				String allowedDir = "/usr/local/virtuoso-opensource/var/lib/virtuoso/db/";
+				dockerClient.copyToContainer(unzipFolder, dockerContainerId, allowedDir);
+
+				logger.info("Connecting to virtuoso");
+				VirtuosoDataSource dataSource = new VirtuosoDataSource();
+				dataSource.setUser("dba");
+				dataSource.setPassword("dba");
+				dataSource.setPortNumber(1111);
+				dataSource.setServerName(dockerContainerIp);
+				try(java.sql.Connection c = dataSource.getConnection()) {
+					logger.info("Preparing bulk loader");
+					VirtuosoBulkLoad.logEnable(c, 2, 0);
+
+					for(Path path : paths) {
+						String actualFilename = path.getFileName().toString();
+						logger.info("Registered file for bulkload: " + actualFilename);
+						VirtuosoBulkLoad.ldDir(c, allowedDir, actualFilename, "http://www.example.org/");
+					}
+					
+					logger.info("Running bulk loader");
+					VirtuosoBulkLoad.rdfLoaderRun(c);
+
+					logger.info("Creating checkpoint");
+					VirtuosoBulkLoad.checkpoint(c);
+				}
+				
+				Files.walk(unzipFolder)
+				    .sorted(Comparator.reverseOrder())
+				    .map(Path::toFile)
+				    //.peek(System.out::println)
+				    .forEach(File::delete);
+				    //.count();
+				
+/// end of DOCKER STUFF
+				
+				String sparqlApiBase = "http://" + ds.getContainerId() + ":8890/";
+				String sparqlEndpoint = sparqlApiBase + "sparql";
+
+				// Wrap the connection to use a different content type for queries...
+				// Jena rejects some of Virtuoso's json output
+				@SuppressWarnings("resource")
+				RDFConnection rawConn = RDFConnectionFactoryEx.connect(sparqlEndpoint, datasetDescription);
+
+				rawConnEx = RDFConnectionFactoryEx.wrap(
+						rawConn,
+						metadata);
+
+			} else {
+				
+				DatasetDescription datasetDescription = new DatasetDescription();
+				datasetDescription.addAllDefaultGraphURIs(cmMain.getDefaultGraphUris());
+	
+				rawConnEx = RDFConnectionFactoryEx.connect(sparqEndpoint, datasetDescription);
+			}				
+			
+			String dataSummaryUri = cmMain.getPathFindingDataSummaryUri();
+			Model dataSummaryModel = null;
+			if(dataSummaryUri != null) {
+				logger.info("Loading path finding data summary from " + dataSummaryUri);
+				dataSummaryModel = RDFDataMgr.loadModel(dataSummaryUri);
+				logger.info("Done loading path finding data summary from" + dataSummaryUri);
+			}
+			
+			
+			RDFConnectionEx conn = rawConnEx;
 		
 			
 			Random random = new Random(0);
@@ -236,6 +339,14 @@ public class MainCliFacetedBrowsingBenchmarkV2TaskGenerator {
 //
 //					Model model = conn.queryConstruct("CONSTRUCT FROM <http://www.example.org/> WHERE { ?s ?p ?o }");
 //					RDFDataMgr.write(System.out, model, RDFFormat.TURTLE_PRETTY);
+		} finally {
+			if(ds != null) {
+				ds.stopAsync().awaitTerminated(30, TimeUnit.SECONDS);
+			}
+
+			if(dss != null) {
+				dss.close();
+			}
 		}
 		
 	}
