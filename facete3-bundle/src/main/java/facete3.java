4a741c5549b64ee58c2a5323a2052b2ed2c414eb
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2019-09-26 02:25:41 +0200

    Added more release plugins

---
+++ facete3-bundle/src/main/java/facete3.java
@@ -0,0 +1,7 @@
+import org.aksw.facete3.cli.main.MainCliFacete3;
+
+public class facete3 {
+	public static void main(String[] args) throws Exception {
+		MainCliFacete3.main(args);
+	}
+}
---
+++ facete3-bundle/src/main/java/fsbg.java
@@ -0,0 +1,7 @@
+import org.hobbit.benchmark.faceted_browsing.v2.main.MainCliFacetedBrowsingBenchmarkV2TaskGenerator;
+
+public class fsbg {
+	public static void main(String[] args) throws Exception {
+		MainCliFacetedBrowsingBenchmarkV2TaskGenerator.main(args);
+	}
+}
