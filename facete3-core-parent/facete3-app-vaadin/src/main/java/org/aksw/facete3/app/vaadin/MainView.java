4fbc686fcb820e9f0b4567ec43b35b6e110970b2
BonaBeavis <gordian.dziwis@gmail.com>
2020-04-07 19:02:59 +0200

    Format MainView.java

--- facete3-core-parent/facete3-app-vaadin/src/main/java/org/aksw/facete3/app/vaadin/MainView.java
+++ facete3-core-parent/facete3-app-vaadin/src/main/java/org/aksw/facete3/app/vaadin/MainView.java
@@ -40,20 +40,15 @@ import com.vaadin.flow.server.PWA;
 /**
  * A sample Vaadin view class.
  * <p>
- * To implement a Vaadin view just extend any Vaadin component and
- * use @Route annotation to announce it in a URL as a Spring managed
- * bean.
- * Use the @PWA annotation make the application installable on phones,
- * tablets and some desktop browsers.
+ * To implement a Vaadin view just extend any Vaadin component and use @Route annotation to announce
+ * it in a URL as a Spring managed bean. Use the @PWA annotation make the application installable on
+ * phones, tablets and some desktop browsers.
  * <p>
- * A new instance of this class is created for every new user and every
- * browser tab/window.
+ * A new instance of this class is created for every new user and every browser tab/window.
  */
 @SuppressWarnings("serial")
 @Route
-@PWA(name = "Vaadin Application",
-        shortName = "Vaadin App",
-        description = "This is an example Vaadin application.",
+@PWA(name = "Vaadin Application", shortName = "Vaadin App", description = "This is an example Vaadin application.",
         enableInstallPrompt = true)
 @CssImport("./styles/shared-styles.css")
 @CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
@@ -62,23 +57,20 @@ public class MainView extends AppLayout {
     public static final char CHAR_UPWARDS_ARROW = '\u2191';
     public static final char CHAR_DOWNWARDS_ARROW = '\u2193';
 
-    public static final String[] sortModeLabel = { "A-Z", "0-9" };
+    public static final String[] sortModeLabel = {"A-Z", "0-9"};
 
     // Mapping of ui sort directions to ui labels
-    public static final String[] sortDirLabel = { Character.toString(CHAR_DOWNWARDS_ARROW), Character.toString(CHAR_UPWARDS_ARROW) };
+    public static final String[] sortDirLabel =
+            {Character.toString(CHAR_DOWNWARDS_ARROW), Character.toString(CHAR_UPWARDS_ARROW)};
 
     // Mapping of ui sort directions to jena query sort directions
-    public static final int[] sortDirMapJena = { Query.ORDER_DESCENDING, Query.ORDER_ASCENDING };
-
+    public static final int[] sortDirMapJena = {Query.ORDER_DESCENDING, Query.ORDER_ASCENDING};
 
     protected PrefixMapping globalPrefixes = new PrefixMappingImpl();
 
     protected boolean showLabels = true;
 
-
-
     public RDFNode resourceViewActiveNode = null;
-
 
     protected FacetedQuery fq;
 
@@ -86,9 +78,10 @@ public class MainView extends AppLayout {
         JenaSystem.init();
         JenaPluginFacete3.init();
 
-        RDFConnection conn = RDFConnectionRemote.create()
+        RDFConnection conn = RDFConnectionRemote
+                .create()
                 .destination("https://databus.dbpedia.org/repo/sparql")
-                //.destination("http://cord19.aksw.org/sparql")
+                // .destination("http://cord19.aksw.org/sparql")
                 .acceptHeaderQuery(WebContent.contentTypeResultsXML)
                 .build();
 
@@ -98,36 +91,39 @@ public class MainView extends AppLayout {
 
         fq = FacetedQueryImpl.create(facetedQuery, conn);
 
-//        fq.root()
-//        .fwd(RDF.type).one()
-//            .constraints()
-//                .eq(DCTerms.BibliographicResource).activate();
+        // fq.root()
+        // .fwd(RDF.type).one()
+        // .constraints()
+        // .eq(DCTerms.BibliographicResource).activate();
     }
 
     public List<String> randomItems() {
         System.out.println("Complex Query");
         List<FacetValueCount> fc =
                 // -- Faceted Browsing API
-                fq.focus()
-                .fwd()
-                .facetValueCounts()
-                .exclude(RDF.type)
-                // --- DataQuery API
-                //.sample()
-                .randomOrder()
-                .limit(10)
-                .peek(x -> System.out.println("GOT: " + x.toConstructQuery()))
-                .exec()
-                // --- RxJava API
-                //.firstElement()
-                .toList()
-                .timeout(60, TimeUnit.SECONDS)
-                .blockingGet();
-        List<String> items = fc.stream().map(x -> (x.getValue() + ": " + x.getFocusCount()).substring(0, 16)).collect(Collectors.toList());
+                fq
+                        .focus()
+                        .fwd()
+                        .facetValueCounts()
+                        .exclude(RDF.type)
+                        // --- DataQuery API
+                        // .sample()
+                        .randomOrder()
+                        .limit(10)
+                        .peek(x -> System.out.println("GOT: " + x.toConstructQuery()))
+                        .exec()
+                        // --- RxJava API
+                        // .firstElement()
+                        .toList()
+                        .timeout(60, TimeUnit.SECONDS)
+                        .blockingGet();
+        List<String> items = fc
+                .stream()
+                .map(x -> (x.getValue() + ": " + x.getFocusCount()).substring(0, 16))
+                .collect(Collectors.toList());
 
         return items;
     }
-
 
     /**
      * Construct a new Vaadin view.
@@ -140,24 +136,24 @@ public class MainView extends AppLayout {
         init();
 
         ListBox<String> facetList = new ListBox<>();
-//        ListBox<String> facetValueList = new ListBox<>();
+        // ListBox<String> facetValueList = new ListBox<>();
 
         facetList.setItems(randomItems());
 
         // VerticalLayout mainPanel = new VerticalLayout();
 
         VerticalLayout facetPanel = new VerticalLayout();
-//        VerticalLayout facetValuePanel = new VerticalLayout();
+        // VerticalLayout facetValuePanel = new VerticalLayout();
         VerticalLayout resultPanel = new VerticalLayout();
 
-        //SplitLayout mainSplit = new SplitLayout();
+        // SplitLayout mainSplit = new SplitLayout();
         VerticalLayout mainPanel = new VerticalLayout();
-//        SplitLayout facetAndValuesSplit = new SplitLayout();
+        // SplitLayout facetAndValuesSplit = new SplitLayout();
 
-//        mainSplit.setOrientation(Orientation.VERTICAL);
+        // mainSplit.setOrientation(Orientation.VERTICAL);
 
-//        facetAndValuesSplit.addToPrimary(facetPanel);
-//        facetAndValuesSplit.addToSecondary(facetValuePanel);
+        // facetAndValuesSplit.addToPrimary(facetPanel);
+        // facetAndValuesSplit.addToSecondary(facetValuePanel);
 
         TextField facetSearchField = new TextField();
         facetSearchField.setWidthFull();
@@ -166,8 +162,8 @@ public class MainView extends AppLayout {
         facetPanel.add(facetSearchField);
         facetPanel.add(facetList);
 
-//        facetValuePanel.add(new Label("Facet Values"));
-//        facetValuePanel.add(facetValueList);
+        // facetValuePanel.add(new Label("Facet Values"));
+        // facetValuePanel.add(facetValueList);
 
         TextField searchField = new TextField();
         searchField.setWidthFull();
@@ -175,10 +171,10 @@ public class MainView extends AppLayout {
 
         resultPanel.add("Results go here");
 
-//        mainPanel.setHeightFull();
-//        mainPanel.setWidthFull();
-//        mainSplit.addToPrimary(facetAndValuesSplit);
-//        mainSplit.addToSecondary(resultPanel);
+        // mainPanel.setHeightFull();
+        // mainPanel.setWidthFull();
+        // mainSplit.addToPrimary(facetAndValuesSplit);
+        // mainSplit.addToSecondary(resultPanel);
 
         mainPanel.add(searchField);
         mainPanel.add(resultPanel);
@@ -188,30 +184,30 @@ public class MainView extends AppLayout {
 
         setPrimarySection(Section.DRAWER);
 
-
-        //addToNavbar(list);
+        // addToNavbar(list);
 
         // Use TextField for standard text input
-//        TextField textField = new TextField("Your name ");
-//
-//        // Button click listeners can be defined as lambda expressions
-//        Button button = new Button("Say hello", e -> {
-//            facetList.setItems(randomItems());
-//            Notification.show(service.greet(textField.getValue()));
-//        });
-//
-//        // Theme variants give you predefined extra styles for components.
-//        // Example: Primary button is more prominent look.
-//        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-//
-//        // You can specify keyboard shortcuts for buttons.
-//        // Example: Pressing enter in this view clicks the Button.
-//        button.addClickShortcut(Key.ENTER);
-//
-//        // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
-//        //addClassName("centered-content");
-//
-//        // addToNavbar(textField, button);
+        // TextField textField = new TextField("Your name ");
+        //
+        // // Button click listeners can be defined as lambda expressions
+        // Button button = new Button("Say hello", e -> {
+        // facetList.setItems(randomItems());
+        // Notification.show(service.greet(textField.getValue()));
+        // });
+        //
+        // // Theme variants give you predefined extra styles for components.
+        // // Example: Primary button is more prominent look.
+        // button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        //
+        // // You can specify keyboard shortcuts for buttons.
+        // // Example: Pressing enter in this view clicks the Button.
+        // button.addClickShortcut(Key.ENTER);
+        //
+        // // Use custom CSS classes to apply styling. This is defined in
+        // shared-styles.css.
+        // //addClassName("centered-content");
+        //
+        // // addToNavbar(textField, button);
     }
 
 }
